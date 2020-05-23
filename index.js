e9d1ba0ae41cdc078ba19473cfb309efed60aea7
beeteedubs <bryanzhu101@gmail.com>
2020-05-22 21:06:13 -0400

    3rd commit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // d3.select("h1").style("color", "red");
+window.onload = function () {
+  let mainObj = {};
+  let theThing = function () {
+    for (let prop in mainObj) {
+      // var output = mainObj.Grandpa1.Name;
+      // document.getElementById("output").innerHTML = output;
+      console.log(prop);
+      console.log(mainObj[prop]);
+    }
+  };
 
-fetch("./data/family_members.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data[0].Husband);
-  });
+  fetch("./data/family_members.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //console.log(data[0].Grandpa1.Name);
+      mainObj = data[0];
+      theThing();
+      var output = mainObj.Grandpa1.Name;
+      document.getElementById("output").innerHTML = output;
+    });
+};
