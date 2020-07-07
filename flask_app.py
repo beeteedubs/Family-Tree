80a0caff35b2e633990b4feb65f116c3a26fcad8
beeteedubs <bryanzhu101@gmail.com>
2020-07-07 14:03:07 -0400

    new changes

--- flask_app.py
+++ flask_app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)  # references this file
 # create index route so browse url don't get 404
 @app.route("/")
 def index():
-    return render_template("index2.html")
-
+    return render_template("extension.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
