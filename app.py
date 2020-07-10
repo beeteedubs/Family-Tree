d2f4f662d9c69162a9d8e466e2c52546529152e3
beeteedubs <bryanzhu101@gmail.com>
2020-07-10 10:42:40 -0400

    incorrect rendering

--- app.py
+++ app.py
@@ -2,46 +2,66 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-# sets up application
-app = Flask(__name__)  # references this file, "turn this file into a web app   "
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "sqlite:///test.db"  # 3 slashes for relative path, 4 for absolute
-db = SQLAlchemy(app)  # initializes database
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app)
 
-# create modle
+
 class Todo(db.Model):
-    id = db.Column(
-        db.Integer, primary_key=True
-    )  # integer that reference sthe id of each entry
-    content = db.Column(db.String(225), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return "<Task %r>" % self.id
 
 
-# create index route so browse url don't get 404
-@app.route(
-    "/", methods=["POST", "GET"]
-)  # default is just get, but now can also post (send data to database)
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST":  # basically when we hit "Add Task"
-        task_content = request.form[
-            "content"
-        ]  # this var = whatever u put in that input text bar
+    if request.method == "POST":
+        task_content = request.form["content"]
         new_task = Todo(content=task_content)
+
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect("/")
         except:
-            return "there was na issue adding your task"
+            return "There was an issue adding your task"
+
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template("extension.html", tasks=tasks)
+        return render_template("index.html", tasks=tasks)
 
 
-# unnecessary
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting that task"
+
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form["content"]
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue updating your task"
+
+    else:
+        return render_template("update.html", task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- functions.py
+++ functions.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-@app.route('/', methods = ['POST','GET'])
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "sqlite:///fam_tree.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["POST", "GET"])
 def data_file():
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(225), nullable = False, default = 'Sasha')
-    parent = db.Column(db.String(225), nullable = False, default = 'Sasha')
-    have_children = db.Column(db.Boolean, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), nullable=False, default="Sasha")
+    parent = db.Column(db.String(225), nullable=False, default="Sasha")
+    have_children = db.Column(db.Boolean, nullable=False)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fam_tree.db"
+
